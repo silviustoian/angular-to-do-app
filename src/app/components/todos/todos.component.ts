f807d22c1fbb40477a907cc3cef41aa9abe2bca1
silviustoian <silviu.stoian96@gmail.com>
2020-08-16 18:13:17 +0300

    delete todos

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -18,4 +18,9 @@ export class TodosComponent implements OnInit {
     });
   }
 
+  deleteTodo(todo:Todo){
+    this.todos = this.todos.filter( t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
 }
